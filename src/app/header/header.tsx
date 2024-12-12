8f44f8a4e36416944cf4293bb7e30763354565b9
Jamie J <jaydee12347@gmail.com>
2024-12-12 03:07:50 -0800

    :sparkles: Feat header with dropdown

---
+++ src/app/header/header.tsx
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.css';
+import Logo from '../logo.ico';
+
+export default function Header() {
+    const about = [
+        { id: 1, name: 'About Us', path: '/' },
+        { id: 2, name: 'Leadership', path: '/' },
+        { id: 3, name: 'History', path: '/' },
+        { id: 4, name: 'Our Covering', path: '/' }
+    ];
+
+    const ministries = [
+        { id: 1, name: 'Inner Healing', path: '/' },
+        { id: 2, name: 'Intercessory Prayer', path: '/' },
+        { id: 3, name: 'Worship', path: '/' }
+    ];
+
+    const planAVisit = [
+        { id: 1, name: 'Contact Us', path: '/' },
+        { id: 2, name: 'Events', path: '/' }
+    ];
+
+    return (
+        <div className={styles['header-container']}>
+            <Image className={styles['header-logo']} src={Logo} alt="Unravel Church Logo" priority />
+
+            <div className={styles['menu-container']}>
+                <div>
+                    <Link className={styles['menu-title']} href="/">Home</Link>
+                </div>
+                
+                <div>
+                    <Link className={styles['menu-title']} href="/">About</Link>
+                    <ul>
+                        {about.map((item) => (
+                            <li key={item.id} className={styles['dropdown']}>
+                                <Link className={styles['dropdown-item']} href={item.path}>{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            
+                <div>
+                    <Link className={styles['menu-title']} href="/">Ministries</Link>
+                    <ul>
+                        {ministries.map((item) => (
+                            <li key={item.id} className={styles['dropdown']}>
+                                <Link className={styles['dropdown-item']} href={item.path}>{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                
+                <div>
+                    <Link className={styles['menu-title']} href="/">Plan a Visit</Link>
+                    <ul>
+                        {planAVisit.map((item) => (
+                            <li key={item.id} className={styles['dropdown']}>
+                                <Link className={styles['dropdown-item']} href={item.path}>{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            
+                <div>
+                    <Link className={styles['menu-title']} href="/">Give</Link>
+                </div>
+            </div>
+           
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Source_Sans_3 } from "next/font/google";
 import "./globals.css";
+import Header from './header/header';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -9,6 +10,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const SourseSans = Source_Sans_3({
+  variable: "--font-sourse-sans",
   subsets: ["latin"],
 });
 
@@ -26,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
       <div className="container">
-          {/* <MainHeader></MainHeader> */}
+          <Header></Header>
           {children}
         </div>
       </body>
