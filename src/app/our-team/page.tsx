ab4f51e0b0806de4d9b2845c8e65c9aef0d0090b
Jamie J <jaydee12347@gmail.com>
2024-12-15 00:09:54 -0800

    :art: style the flex box

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -22,8 +22,8 @@ export default function OurTeam(){
                     </div>
                 </div>
 
-                <div className={styles['team-desc']}>
-                    <Image className={styles['team-img']} src={pastor} alt="Picture of our pastor" />
+                <div className={styles['team-desc-two']}>
+                    <Image className={styles['team-img-two']} src={pastor} alt="Picture of our pastor" />
                     <div>
                         <h4>Pastor B</h4>
                         <p>Hello, my name is B, and I am blessed to serve as the pastor of Unravel Church. With a heart for ministry and a passion for spreading God&#39;s love, I aim to guide and support our congregation in faith, hope, and spiritual growth. I look forward to walking alongside each of you in this journey of faith.</p>
@@ -38,8 +38,8 @@ export default function OurTeam(){
                     </div>
                 </div>
 
-                <div className={styles['team-desc']}>
-                    <Image className={styles['team-img']} src={reverend} alt="Picture of our reverend" />
+                <div className={styles['team-desc-two']}>
+                    <Image className={styles['team-img-two']} src={reverend} alt="Picture of our reverend" />
                     <div>
                         <h4>Reverend B</h4>
                         <p>Greetings, I am Reverend B, and I am honored to serve the community of Unravel Church. With a deep commitment to faith, service, and spiritual growth, I look forward to walking with you on this journey of God's love and grace.</p>
@@ -54,8 +54,8 @@ export default function OurTeam(){
                     </div>
                 </div>
                 
-                <div className={styles['team-desc']}>
-                    <Image className={styles['team-img']} src={minister} alt="Picture of our minister" />
+                <div className={styles['team-desc-two']}>
+                    <Image className={styles['team-img-two']} src={minister} alt="Picture of our minister" />
                     <div>
                         <h4>Minister A</h4>
                         <p>Hello, I&#39;m Minister A, and it&#39;s a privilege to serve alongside you in faith. My goal is to help nurture our spiritual growth and strengthen our community through God&#39;s love and guidance. I look forward to walking with you on this journey of faith and service.</p>
