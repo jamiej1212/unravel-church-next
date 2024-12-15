f377c466823c8b4c938e0e11f844568c544ec463
Jamie J <jaydee12347@gmail.com>
2024-12-15 03:12:39 -0800

    :sparkles: Feat contact us page structures and styles

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,55 @@
+import Banner from '../components/banner';
+
+import Image from 'next/image';
+import contactImg from '../assets/contactus.jpg';
+
+import styles from './page.module.css';
+
+export default function ContactUs(){
+    return(
+        <>
+            <Banner prop="CONTACT US" />
+            <div className={styles['contact-container']}>
+                <Image className={styles['contact-img']} src={contactImg} alt="Scenery image"/>
+                <form className={styles['contact-form']}>
+                    <h2>Contact Us</h2>
+                    <label htmlFor="full-name">Full Name *</label>
+                    <input
+                        className={styles['input-field']}
+                        type="text" 
+                        name="full-name" 
+                        placeholder="Your Name"
+                        required 
+                    />
+
+                    <label htmlFor="email-address">Email Address *</label>
+                    <input 
+                        className={styles['input-field']}
+                        type="email" 
+                        name="email-address" 
+                        pattern="[^@]+@[^@]+\.[a-zA-Z0-9-.]+" 
+                        placeholder="hello@gmail.com"
+                        required 
+                    />
+                    
+                    <label htmlFor="phone-number">Phone Number</label>
+                    <input 
+                        className={styles['input-field']}
+                        type="tel" 
+                        name="phone-number"
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                        placeholder="123-456-7890"
+                        required
+                    />
+
+                    <label htmlFor="message">Enter Message</label>
+                    <textarea
+                        placeholder="Enter Message"
+                    />
+
+                    <input className={styles.submit} type="submit" value="Submit" />
+                </form>
+            </div>
+        </>
+    );
+}
