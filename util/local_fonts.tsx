b6c183d00e3913e2059f9c255c990752af576ee0
Jamie J <jaydee12347@gmail.com>
2024-12-25 11:51:37 -0800

    🔥 Remove unnecessary folder

--- util/local_fonts.tsx
+++
@@ -1,8 +0,0 @@
-import localFont from "next/font/local";
-
-const Pretendard = localFont({
-    src: [{ path: '../../public/fonts/PretendardVariable.ttf'}],
-    variable: "--font-pretendard"
-});
-
-export { Pretendard };
